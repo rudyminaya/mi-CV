5ffd69f23ba9254f0df1dcee3a64b069e550f54d
Rudy Minaya <rudy.minaya@copsotne.net>
2021-01-02 14:07:37 -0500

    iniciando titulos

--- components/Titulos.jsx
+++ components/Titulos.jsx
@@ -1,1 +1,10 @@
 import React from 'react';
+const Titulo = (props) => {
+    return (
+        <div>
+            <h1>{props.textoEstatico}</h1><h1>{props.textoAnimado}</h1>
+        </div>
+    )
+}
+
+export default Titulo;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import { FaDivide } from 'react-icons/fa'
 import Button from '../components/Button'
+import Titulo from '../components/Titulos'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
         <div>
-
+            <Titulo textoEstatico="Hola" textoAnimado="soy yo"></Titulo>
         </div>
     )
 }
